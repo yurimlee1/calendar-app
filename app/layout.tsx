94b06953b6d1dc8ae70f4677f9601f9f26f8233d
N13 <l.lauren@N13-MacBook-Pro.local>
2025-01-13 18:34:48 -0500

    updated layout
    resolved favicon error
    
    getting an error, hydrateRoot: react.dev/link/hydration-mismatch

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="#" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
