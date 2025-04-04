233acf1b8b7f67b7dbfebef76671fdb6438713fe
yurimlee1 <yurim.lee.123@gmail.com>
2025-04-04 15:17:38 -0400

    updated layout.tsx and postcss.config
    commented font definition out and changed postcss.config back to .mjs format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+//
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Calendar Web Application",
@@ -29,7 +29,8 @@ export default function RootLayout({
         <link rel="shortcut icon" href="#" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`antialiased`}
       >
         <ThemeProvider
           attribute="class"
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -5,5 +5,5 @@ const config = {
   },
 };
 
-// export default config;
-module.exports = config;
+export default config;
+// module.exports = config;
